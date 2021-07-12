3d4fa26d10fed42c5add2aec1c83e956dbf1a026
Reuben Jacob Mathew <rjmat97@gmail.com>
2021-07-12 17:50:41 +0530

    aggregation ready

--- server/index.js
+++ server/index.js
@@ -12,10 +12,6 @@ app.get('/', (req, res) => {
     mongo.main().then(val => {
         res.send(val)
     })
-    
-    // main().then(val => res.send(val))
-    // console.log(val)
-    // res.send(val)
 })
 app.get('/home', (req, res) => {
     res.sendFile(path.join(__dirname,'/index.html'))
--- server/mongo.js
+++ server/mongo.js
@@ -5,48 +5,52 @@ module.exports.main = async function (){
     const client = new MongoClient(uri,{ useNewUrlParser: true, useUnifiedTopology: true })
     try{
         await client.connect()
-        return await showDBs(client)
+        // return await showDBs(client)
         // return await find(client)
-        // findOne(client, "A2a")
-        // return "hello there"
+        // return await find(client, "A2a")
+        return await aggregate(client)
     } catch(e) {
         console.error(e)
     } finally {
         await client.close()
     }
 }
-async function showDBs(client){
-    databasesList = await client.db().admin().listDatabases()
-    // let retStr = {}
-    // databasesList.databases.forEach(db => retStr = retStr + ` - ${db.name}\n`)
-    return databasesList.databases.map(db => db.name)  
-};
 
-async function insertOne(client, newListing){
-    // const drop   = await client.db("gear19").collection("states").drop()
-    const result = await client.db("gear19").collection("states").insertOne(newListing)
+async function aggregate(client){
+    const ob = [
+        {$project: {_id: "$accession", variant:"$clade", date: "$date"}},
+        {$group:   {_id: {date:"$date",variant:"$variant"}, count:{$sum:1}}}
+    ]
+    return await client.db("gear19").collection("states").aggregate(ob).toArray()
 }
+// async function showDBs(client){
+//     databasesList = await client.db().admin().listDatabases()
+//     // let retStr = {}
+//     // databasesList.databases.forEach(db => retStr = retStr + ` - ${db.name}\n`)
+//     return databasesList.databases.map(db => db.name)  
+// };
 
-async function insertMany(client, newListing){
-    const result = await client.db("gear19").collection("states").insertMany(newListing)
-}
+// async function insertOne(client, newListing){
+//     // const drop   = await client.db("gear19").collection("states").drop()
+//     const result = await client.db("gear19").collection("states").insertOne(newListing)
+// }
+
+// async function insertMany(client, newListing){
+//     const result = await client.db("gear19").collection("states").insertMany(newListing)
+// }
 
 async function findOne(client, search){
     const result = await client.db("gear19").collection("states").findOne({clade: search})
-    if (result) {
-        console.log(`Found a listing in the collection with the name '${search}':`);
-        console.log(result);
-    } else {
-        console.log(`No listings found with the name '${search}'`);
-    }
+    if (result) return result
+    else return 0
 }
 
-async function find(client){
-    const result = await client.db("gear19").collection("states").find({clade: "A2a"})
-    console.log(result)
-    if(result){
-        return result
-    } else {
-        return 0
-    }
-}
+// async function find(client){
+//     const result = await client.db("gear19").collection("states").find({clade: "A2a"})
+//     console.log(result)
+//     if(result){
+//         return result
+//     } else {
+//         return 0
+//     }
+// }
